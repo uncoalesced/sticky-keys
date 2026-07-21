92622d0e869eda3f4d3b4833fdaecd88ba0e23d4
jvag15 <uncoalesced@gmail.com>
2026-07-21 23:14:41 +0530

    build(ksp): update StickersViewModel factory source

--- app/build/generated/ksp/debug/java/com/uncoalesced/stickykeys/ui/screens/StickersViewModel_Factory.java
+++ app/build/generated/ksp/debug/java/com/uncoalesced/stickykeys/ui/screens/StickersViewModel_Factory.java
@@ -1,7 +1,9 @@
 package com.uncoalesced.stickykeys.ui.screens;
 
+import com.uncoalesced.stickykeys.stickercore.domain.repository.StickerRepository;
 import dagger.internal.DaggerGenerated;
 import dagger.internal.Factory;
+import dagger.internal.Provider;
 import dagger.internal.QualifierMetadata;
 import dagger.internal.ScopeMetadata;
 import javax.annotation.processing.Generated;
@@ -23,20 +25,22 @@ import javax.annotation.processing.Generated;
     "nullness:initialization.field.uninitialized"
 })
 public final class StickersViewModel_Factory implements Factory<StickersViewModel> {
+  private final Provider<StickerRepository> repositoryProvider;
+
+  private StickersViewModel_Factory(Provider<StickerRepository> repositoryProvider) {
+    this.repositoryProvider = repositoryProvider;
+  }
+
   @Override
   public StickersViewModel get() {
-    return newInstance();
+    return newInstance(repositoryProvider.get());
   }
 
-  public static StickersViewModel_Factory create() {
-    return InstanceHolder.INSTANCE;
+  public static StickersViewModel_Factory create(Provider<StickerRepository> repositoryProvider) {
+    return new StickersViewModel_Factory(repositoryProvider);
   }
 
-  public static StickersViewModel newInstance() {
-    return new StickersViewModel();
-  }
-
-  private static final class InstanceHolder {
-    static final StickersViewModel_Factory INSTANCE = new StickersViewModel_Factory();
+  public static StickersViewModel newInstance(StickerRepository repository) {
+    return new StickersViewModel(repository);
   }
 }
