fb7b76b749e4e4ff1ea0d2403682531be450da1e
jvag15 <uncoalesced@gmail.com>
2026-07-22 00:24:43 +0530

    feat(keyboard-core): add KeyboardState management

---
+++ keyboard-core/src/main/java/com/uncoalesced/stickykeys/keyboardcore/ime/KeyboardState.kt
@@ -0,0 +1,10 @@
+// Engineered by uncoalesced
+package com.uncoalesced.stickykeys.keyboardcore.ime
+
+enum class KeyboardMode {
+    LETTERS_LOWER, LETTERS_UPPER, LETTERS_CAPS_LOCK, SYMBOLS, SYMBOLS_SHIFTED
+}
+
+enum class AppMode {
+    TYPING, STICKERS
+}
