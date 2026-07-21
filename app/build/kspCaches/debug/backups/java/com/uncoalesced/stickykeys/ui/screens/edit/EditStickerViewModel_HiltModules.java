14579307bdd7e4c203da0a59473f4419f4127386
jvag15 <uncoalesced@gmail.com>
2026-07-21 23:14:52 +0530

    build(cache): update KSP debug and release backup sources and symbol lookup tables

---
+++ app/build/kspCaches/debug/backups/java/com/uncoalesced/stickykeys/ui/screens/edit/EditStickerViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.uncoalesced.stickykeys.ui.screens.edit;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = EditStickerViewModel.class
+)
+public final class EditStickerViewModel_HiltModules {
+  private EditStickerViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(EditStickerViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(EditStickerViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(EditStickerViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
